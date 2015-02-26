95dfccea33bd2dff8c4e461642839b846a70abdf
Jarrell Waggoner <jwaggoner@groupon.com>
2015-02-26 09:46:43 -0600

    version bump + rename for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject honeysql "0.4.3"
+(defproject com.malloc47/honeysql "0.4.3-p1"
   :description "SQL as Clojure data structures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
